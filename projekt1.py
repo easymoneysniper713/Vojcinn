fb0fe83a33752c7e3276ab109dd3858137d66504
Vojtěch Juroszek <vojtech.juroszek@tria-tr.cz>
2026-03-09 09:05:57 +0100

    uprava

---
+++ projekt1.py
@@ -0,0 +1,4 @@
+print("Ahoj světe!")
+print("Toto je můj první Python projekt.")
+print("Python je skvělý jazyk pro začátečníky i pokročilé programátory.")
+print("Těším se na další projekty v Pythonu!")
