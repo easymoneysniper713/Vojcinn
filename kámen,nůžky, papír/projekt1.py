c0c3a3239d2bd1254e396c4aa92b8d230ced0d33
vojcinn <vojtech.juroszek@tria-tr.cz>
2026-03-10 21:44:30 +0100

    Delete kámen,nůžky, papír/projekt1.py

--- kámen,nůžky, papír/projekt1.py
+++
@@ -1,5 +0,0 @@
-# Projekt 
-print("Ahoj světe!")
-print("Toto je můj první Python projekt.")
-print("Python je skvělý jazyk pro začátečníky i pokročilé programátory.")
-print("Těším se na další projekty v Pythonu!")
