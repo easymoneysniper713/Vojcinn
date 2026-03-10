7cb3bd5bd9529e557c251f87f1ac633dcd2938cc
vojcinn <vojtech.juroszek@tria-tr.cz>
2026-03-10 21:28:45 +0100

    kámen, nůžky, papír

--- projekt1.py
+++ projekt1.py
@@ -1,5 +1,27 @@
-# Projekt 
-print("Ahoj světe!")
-print("Toto je můj první Python projekt.")
-print("Python je skvělý jazyk pro začátečníky i pokročilé programátory.")
-print("Těším se na další projekty v Pythonu!")
+import random
+
+choices = ["kamen", "nuzky", "papir"]
+
+while True:
+    player = input("Vyber kamen, nuzky, papir (nebo napis konec): ").lower()
+
+    if player == "konec":
+        print("Konec hry.")
+        break
+
+    computer = random.choice(choices)
+
+    print("Počítač vybral:", computer)
+
+    if player == computer:
+        print("Remíza")
+    elif player == "kamen" and computer == "nuzky":
+        print("Vyhrál jsi!")
+    elif player == "nuzky" and computer == "papir":
+        print("Vyhrál jsi!")
+    elif player == "papir" and computer == "kamen":
+        print("Vyhrál jsi!")
+    else:
+        print("Prohrál jsi!")
+
+    print()  # prázdný řádek
