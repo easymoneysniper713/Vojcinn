679261e789fafd6f5d97ad43b20df777c6e00195
vojcinn <vojtech.juroszek@tria-tr.cz>
2026-03-10 21:42:31 +0100

    Add files via upload

---
+++ kámen,nůžky, papír/projekt1.py
@@ -0,0 +1,5 @@
+# Projekt 
+print("Ahoj světe!")
+print("Toto je můj první Python projekt.")
+print("Python je skvělý jazyk pro začátečníky i pokročilé programátory.")
+print("Těším se na další projekty v Pythonu!")
